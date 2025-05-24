11047cc072429b8abaa2d7ee9a833ee3bebc1591
minhulee <dlalsgns9039@gmail.com>
2025-05-24 18:39:29 +0900

    Modify : configuration and tweak *.ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
-import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, MeshBuilder } from "@babylonjs/core";
-import "@babylonjs/inspector";
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  HemisphericLight,
+  Vector3,
+  MeshBuilder,
+} from '@babylonjs/core';
+import '@babylonjs/inspector';
 
 // window.addEventListener("DOMContentLoaded", () => {
 // 	const canvas = document.createElement("canvas");
 // })
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   // canvas 생성 및 DOM에 추가
-  const canvas = document.createElement("canvas");
-  canvas.style.width = "100%";
-  canvas.style.height = "100%";
-  canvas.id = "renderCanvas";
+  const canvas = document.createElement('canvas');
+  canvas.style.width = '100%';
+  canvas.style.height = '100%';
+  canvas.id = 'renderCanvas';
   document.body.appendChild(canvas);
 
   const engine = new Engine(canvas, true);
@@ -18,17 +25,24 @@ window.addEventListener("DOMContentLoaded", () => {
   const createScene = () => {
     const scene = new Scene(engine);
 
-    const camera = new ArcRotateCamera("camera", Math.PI / 2, Math.PI / 3, 10, Vector3.Zero(), scene);
+    const camera = new ArcRotateCamera(
+      'camera',
+      Math.PI / 2,
+      Math.PI / 3,
+      10,
+      Vector3.Zero(),
+      scene,
+    );
     camera.attachControl(canvas, true);
 
-    const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
+    const light = new HemisphericLight('light', new Vector3(1, 1, 0), scene);
     light.intensity = 0.9;
 
-    const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
-	const sphere2 = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
+    const sphere = MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene);
+    const sphere2 = MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene);
     sphere.position.y = 0.8;
 
-    MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+    MeshBuilder.CreateGround('ground', { width: 10, height: 10 }, scene);
 
     return scene;
   };
@@ -40,7 +54,7 @@ window.addEventListener("DOMContentLoaded", () => {
     scene.render();
   });
 
-  window.addEventListener("resize", () => {
+  window.addEventListener('resize', () => {
     engine.resize();
   });
 });
